3457fcf69c81b47a88bad3f060b9083c51b90121
motephyr <motephyr@gmail.com>
2014-01-01 23:34:57 +0800

    update bootstrap 3.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 RailsUsefulLink::Application.routes.draw do
   devise_for :users
   resources :posts
+  resources :demos
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
