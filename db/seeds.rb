322e25bf56eb5715f95cafbe4bca7df9ffc0d7d0
motephyr <motephyr@gmail.com>
2014-01-13 02:03:00 +0800

    set seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = [   
+  {id:1, name:"Gem"},   
+  {id:2, name:"新聞"},   
+  {id:3, name:"討論"},   
+  {id:4, name:"教學"} 
+] 
+categories.each do |category| 
+  a = Category.where(:id => category[:id])
+  if a.empty?
+    s = Category.new category   
+    s.id = category[:id]   
+    s.save
+  end
+end 
