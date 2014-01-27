42c4f0a8e5785c9269f731a819b4903f8d34b2f7
motephyr <motephyr@gmail.com>
2014-01-28 02:19:22 +0800

    設定畫面

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,10 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-RailsUsefulLink::Application.config.secret_key_base = 'abc'
+
+if Rails.env.test? || Rails.env.development? || Rails.env == "profile"
+  RailsUsefulLink::Application.config.secret_token = "7ab148a821ef651678105048552f246848efa2add3a23ece510085a183f4044475258dd5d5ce1fb3bf28c871b1d924b4d9d48f38138726ec1c8d8d2c053d9f25"
+else
+  raise "You must set a secret token in ENV['SECRET_TOKEN'] or in config/initializers/secret_token.rb" if ENV['SECRET_TOKEN'].blank?
+  RailsUsefulLink::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
